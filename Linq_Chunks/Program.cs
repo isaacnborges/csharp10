b75265193c3cac8581e57d3e2ef5533f706406d5
Isaac Nunes Borges <isaac.borges@avenuecode.com>
2021-10-25 10:58:32 -0300

    Add example chunk

---
+++ Linq_Chunks/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("*** Example Chunk ***");
+
+var numbers = Enumerable.Range(1, 1000);
+var chunks = numbers.Chunk(100);
+
+foreach (var chunk in chunks)
+{
+    Console.WriteLine($"{chunk.First()}...{chunk.Last()}");
+}
--- Linq_ExceptBy-DistinctBy/Program.cs
+++ Linq_ExceptBy-DistinctBy/Program.cs
@@ -1,18 +1,6 @@
 ﻿using System.Text.Json;
 
-namespace Linq_ExceptBy_DistinctBy
-{
-    public class Pais
-    {
-        public string? Nome { get; set; }
-        public string? Continente { get; set; }
-    }
-
-    class Program
-    {
-        static void Main()
-        {
-            var paises = new Pais[]{
+var paises = new Pais[]{
                 new () { Nome = "Alemanha", Continente = "Europa" },
                 new () { Nome = "Australia", Continente = "Oceania" },
                 new () { Nome = "Brasil", Continente = "America do Sul" },
@@ -27,43 +15,46 @@ namespace Linq_ExceptBy_DistinctBy
                 new () { Nome = "Russia", Continente = "Europa" },
             };
 
-            var paisesUniaoEuropeia = new Pais[]{
+var paisesUniaoEuropeia = new Pais[]{
                 new () { Nome = "Alemanha", Continente = "Europa" },
                 new () { Nome = "Espanha", Continente = "Europa" },
                 new () { Nome = "Franca", Continente = "Europa" },
                 new () { Nome = "Italia", Continente = "Europa" }
             };
 
-            Console.WriteLine($"No. de elementos nos arrays: " +
+Console.WriteLine($"No. de elementos nos arrays: " +
                 $"{nameof(paises)} = {paises.Length}, " +
                 $"{nameof(paisesUniaoEuropeia)} = {paisesUniaoEuropeia.Length} ");
 
-            var paisesExcetoUniaoEuropeia = paises.ExceptBy(paisesUniaoEuropeia.Select(x => x.Nome), p => p.Nome);
-            ExibirResultado("Testes com ExceptBy (ignorando países da Uniao Europeia)", paisesExcetoUniaoEuropeia);
+var paisesExcetoUniaoEuropeia = paises.ExceptBy(paisesUniaoEuropeia.Select(x => x.Nome), p => p.Nome);
+ExibirResultado("Testes com ExceptBy (ignorando países da Uniao Europeia)", paisesExcetoUniaoEuropeia);
 
-            var paisesParcial = paises.Union(paisesUniaoEuropeia);
-            Console.WriteLine($"No. de elementos no Enumerable {nameof(paisesParcial)} = {paisesParcial.Count()}");
+var paisesParcial = paises.Union(paisesUniaoEuropeia);
+Console.WriteLine($"No. de elementos no Enumerable {nameof(paisesParcial)} = {paisesParcial.Count()}");
 
-            var paisesDistinct = paisesParcial.DistinctBy(p => p.Nome);
-            ExibirResultado($"Testes com DistinctBy (todos os países distintos em {nameof(paisesDistinct)})", paisesDistinct);
-        }
+var paisesDistinct = paisesParcial.DistinctBy(p => p.Nome);
+ExibirResultado($"Testes com DistinctBy (todos os países distintos em {nameof(paisesDistinct)})", paisesDistinct);
 
-        static void ExibirResultado(string tipoTeste, IEnumerable<Pais> paises)
-        {
-            Console.WriteLine();
-            Console.WriteLine($"*** {tipoTeste} ***");
-            Console.WriteLine();
+static void ExibirResultado(string tipoTeste, IEnumerable<Pais> paises)
+{
+    Console.WriteLine();
+    Console.WriteLine($"*** {tipoTeste} ***");
+    Console.WriteLine();
 
-            Console.WriteLine($"No. de elementos: {paises.Count()}");
-            Console.WriteLine();
+    Console.WriteLine($"No. de elementos: {paises.Count()}");
+    Console.WriteLine();
 
-            Console.WriteLine($"Dados: {JsonSerializer.Serialize(paises)}");
-            Console.WriteLine();
+    Console.WriteLine($"Dados: {JsonSerializer.Serialize(paises)}");
+    Console.WriteLine();
 
-            Console.WriteLine("Países:");
-            foreach (var pais in paises)
-                Console.WriteLine($"  * {pais.Nome} ({pais.Continente})");
-            Console.WriteLine();
-        }
-    }
+    Console.WriteLine("Países:");
+    foreach (var pais in paises)
+        Console.WriteLine($"  * {pais.Nome} ({pais.Continente})");
+    Console.WriteLine();
+}
+
+public class Pais
+{
+    public string? Nome { get; set; }
+    public string? Continente { get; set; }
 }
